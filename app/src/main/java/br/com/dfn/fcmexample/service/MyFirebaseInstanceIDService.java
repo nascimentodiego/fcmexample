9f959e1e7221366ae7a06a691de3a510bd8f9a6c
Diego Nascimento <dfn@cesar.org.br>
2016-09-02 10:59:21 -0300

    init project

---
+++ app/src/main/java/br/com/dfn/fcmexample/service/MyFirebaseInstanceIDService.java
@@ -0,0 +1,27 @@
+package br.com.dfn.fcmexample.service;
+
+import android.util.Log;
+import android.widget.Toast;
+
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.FirebaseInstanceIdService;
+
+public class MyFirebaseInstanceIDService extends FirebaseInstanceIdService {
+
+    private static String TAG = MyFirebaseInstanceIDService.class.getName();
+
+    public MyFirebaseInstanceIDService() {
+    }
+
+    @Override
+    public void onTokenRefresh() {
+        // Get updated InstanceID token.
+        String refreshedToken = FirebaseInstanceId.getInstance().getToken();
+        Log.d(TAG, "Refreshed token: " + refreshedToken);
+
+        // TODO: Implement this method to send any registration to your app's servers.
+        //sendRegistrationToServer(refreshedToken);
+        Toast.makeText(MyFirebaseInstanceIDService.this, "Token: " + refreshedToken , Toast.LENGTH_SHORT).show();
+
+    }
+}
